90e6b4f283a158be210e61e8de238db85f7d4e77
Vesselin Portev <veskoap@icloud.com>
2025-05-28 02:21:14 -0400

    Update Sentry configuration to use environment variables for organization and project settings, enhancing flexibility and security in deployment.

--- next.config.mjs
+++ next.config.mjs
@@ -16,8 +16,8 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: 'spoqen',
-  project: 'javascript-nextjs',
+  org: process.env.SENTRY_ORG || 'spoqen',
+  project: process.env.SENTRY_PROJECT || 'javascript-nextjs',
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
