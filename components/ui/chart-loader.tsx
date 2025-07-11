1cc7208d1a67788c3453b921a82215e98f4ea03f
Vesselin Portev <veskoap@icloud.com>
2025-07-11 03:59:04 -0400

    chore: formatting

--- components/ui/chart-loader.tsx
+++ components/ui/chart-loader.tsx
@@ -19,11 +19,30 @@ import type {
  *   } from '@/components/ui/chart-loader';
  */
 
-export const ChartContainer = dynamic(() => import('./chart').then(m => m.ChartContainer), { ssr: false });
-export const ChartTooltip = dynamic(() => import('./chart').then(m => m.ChartTooltip as unknown as ComponentType<any>), { ssr: false });
-export const ChartTooltipContent = dynamic(() => import('./chart').then(m => m.ChartTooltipContent), { ssr: false });
-export const ChartLegend = dynamic(() => import('./chart').then(m => m.ChartLegend as unknown as ComponentType<any>), { ssr: false });
-export const ChartLegendContent = dynamic(() => import('./chart').then(m => m.ChartLegendContent), { ssr: false });
+export const ChartContainer = dynamic(
+  () => import('./chart').then(m => m.ChartContainer),
+  { ssr: false }
+);
+export const ChartTooltip = dynamic(
+  () =>
+    import('./chart').then(
+      m => m.ChartTooltip as unknown as ComponentType<any>
+    ),
+  { ssr: false }
+);
+export const ChartTooltipContent = dynamic(
+  () => import('./chart').then(m => m.ChartTooltipContent),
+  { ssr: false }
+);
+export const ChartLegend = dynamic(
+  () =>
+    import('./chart').then(m => m.ChartLegend as unknown as ComponentType<any>),
+  { ssr: false }
+);
+export const ChartLegendContent = dynamic(
+  () => import('./chart').then(m => m.ChartLegendContent),
+  { ssr: false }
+);
 
 // Default export: container (most common case).
 export default ChartContainer;
